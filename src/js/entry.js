aa6bf88d18fe648d989114cda9fc59cfb71f7da6
Naokazu Shimabukuro <shimabukuro@pnrm.co.jp>
2019-10-11 15:34:07 +0900

    ボードがマウスに追従する

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,4 +1,12 @@
 import * as PIXI from 'pixi.js';
+import Vector2 from './Vector2';
+
+const APP_WIDTH = 600;
+const APP_HEIGHT = 480;
+
+const clamp = (value, min, max) => {
+  return Math.min(max, Math.max(min, value));
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   const wrapper = document.querySelector('.js-canvas');
@@ -51,11 +59,22 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   const tick = () => {
-    // ball.x = ball.x + 1;
-    // ball.y = ball.y + 1;
+    board.x = clamp(board.x + (userPointer.x - board.x) * 0.1, board.width * 0.5, APP_WIDTH - board.width * 0.5);
+    board.y = APP_HEIGHT - 50;
+  };
 
-    board.x = board.x + 1;
-    board.y = board.x + 2;
+  const setEvents = () => {
+    app.stage.on('pointermove', handlePointerMove);
+  };
+
+  const handlePointerMove = evt => {
+    const { x, y } = evt.data.global;
+
+    const xc = clamp(x, 0, APP_WIDTH);
+    const yc = clamp(y, 0, APP_HEIGHT);
+
+    userPointer.x = xc;
+    userPointer.y = yc;
   };
 
   // ブロック崩しのボール
@@ -63,12 +82,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // ボールを打つ板
   const board = drawRect(100, 10);
+  board.pivot.set(50, 5);
 
+  // ユーザーのマウス位置を保存します
+  const userPointer = new Vector2(0, 0);
+
+  app.stage.interactive = true;
   app.stage.addChild(ball);
   app.stage.addChild(board);
 
   // 描画 Canvas を DOM に挿入
   wrapper.appendChild(app.view);
+
+  setEvents();
 
   // アップデート関数をtickerに登録する
   PIXI.Ticker.shared.add(tick);
